5d6188220eed4e8c5a9b6372c98d10e4d46a7e28
appstecnologia <cleiton.araujo@appstecnologia.com>
2023-10-22 23:05:32 -0300

    ok

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $hora_atual = date('H:i:s');
                             <a class="nav-link active" aria-current="page" id='contatomenu' href="#">Contato</a>
                         </li>
                         <li class="nav-item" style="display: none;" id='admmenu'>
-                            <a class="nav-link active" aria-current="page" id='contatomenu' href="https://appstecnologia.com/admin"> Admin </a>
+                            <a class="nav-link active" aria-current="page" id='contatomenu' href="https://admin.appstecnologia.com"> Admin </a>
                         </li>
 
                     </ul>
